8586d23f00972d825fc76c75e9be10b99c0dc4f1
imrichardwu <richard9@ualberta.ca>
2025-12-05 22:36:33 -0700

    p2

--- day6/Compactor.py
+++ day6/Compactor.py
@@ -11,19 +11,86 @@ class Solution:
                 for num in nums:
                     total *= num
             res += total
+        return res
     
-    def p2(self, problems):
+    def p2(self, num_lines, op_line, max_len):
+        # For Part 2, we read right-to-left, one column at a time
+        # Each number is in its own column, with most significant digit at top, least at bottom
+        # Read each column top-to-bottom to get the number
+        
+        # Find separator columns (columns that are all spaces in number lines)
+        separator_cols = []
+        for col in range(max_len):
+            is_separator = True
+            for line in num_lines:
+                if col < len(line) and line[col] != ' ':
+                    is_separator = False
+                    break
+            if is_separator:
+                separator_cols.append(col)
+        
+        # Process problems from right to left
+        problems = []
+        
+        # Add boundaries
+        all_boundaries = [0] + separator_cols + [max_len]
+        
+        # Process each problem region from right to left
+        for i in range(len(all_boundaries) - 1, 0, -1):
+            end_col = all_boundaries[i]
+            start_col = all_boundaries[i-1]
+            
+            if end_col <= start_col:
+                continue
+            
+            # Extract numbers from this problem region
+            # Read columns from right to left, each column is a number read top-to-bottom
+            problem_nums = []
+            
+            for col in range(end_col - 1, start_col - 1, -1):
+                # Read this column from top to bottom
+                num_str = ''
+                for line in num_lines:
+                    if col < len(line) and line[col].isdigit():
+                        num_str += line[col]
+                    elif col < len(line) and line[col] != ' ':
+                        # Non-digit, non-space character - might be part of formatting
+                        pass
+                
+                # If we found digits in this column, it's a number
+                if num_str:
+                    try:
+                        problem_nums.append(int(num_str))
+                    except ValueError:
+                        pass
+            
+            # Reverse to get numbers in left-to-right order (since we processed right-to-left)
+            problem_nums = list(reversed(problem_nums))
+            
+            # Get the operator (rightmost operator in this region)
+            op = None
+            for col in range(end_col - 1, start_col - 1, -1):
+                if col < len(op_line) and op_line[col] in '+*':
+                    op = op_line[col]
+                    break
+            
+            if problem_nums and op:
+                problems.append((problem_nums, op))
+        
+        # Calculate total
         res = 0
         for nums, operator in problems:
-            col = max(nums)
-            # read num from right to left top to bottom
+            if not nums:
+                continue
             if operator == '+':
                 total = sum(nums)
             elif operator == '*':
                 total = 1
                 for num in nums:
                     total *= num
-                    
+            res += total
+        return res
+        
 
 
 if __name__ == '__main__':
@@ -108,4 +175,5 @@ if __name__ == '__main__':
             problems.append((problem_nums, op))
     
     sol = Solution()
-    sol.p1(problems)
+    print("Part 1:", sol.p1(problems))
+    print("Part 2:", sol.p2(num_lines, op_line, max_len))
